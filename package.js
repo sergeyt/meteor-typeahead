651f6b9ff9dd5cd7906e6514fac40b0307274ef1
sergeyt <stodyshev@gmail.com>
2014-09-29 23:13:39 +0700

    fixing issue #33

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
 	name: "sergeyt:typeahead",
 	summary: "Autocomplete package for meteor powered by twitter typeahead.js",
 	git: "https://github.com/sergeyt/meteor-typeahead.git",
-    version: "0.10.5_1"
+    version: "0.10.5_2"
 });
 
 if (typeof Package.onUse == "function") { // for meteor v0.9+
 	Package.onUse(function(api) {
 		api.versionsFrom('METEOR@0.9.0');
-		api.use(['jquery', 'blaze', 'ui'], 'client');
+		api.use(['jquery', 'blaze@1.0.0 || 2.0.0', 'ui'], 'client');
 		api.addFiles('typeahead.bundle.js', 'client');
 		api.addFiles('index.js', 'client');
 	});
