3c3675c5a5c678de898c49b8becbfc90b6fb125f
sergeyt <stodyshev@gmail.com>
2015-01-24 15:38:38 +0600

    fixed weak dep on bootstrap (PR #51)

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
 	api.versionsFrom('METEOR@1.0');
 	api.use(['jquery', 'blaze@1.0.0 || 2.0.0', 'ui'], 'client');
-	api.use('bootstrap', 'client', {weak: true});
+	api.use('twbs:bootstrap@3.0.0', 'client', {weak: true});
 	api.addFiles('typeahead.bundle.js', 'client');
 	api.addFiles('index.js', 'client');
 });
