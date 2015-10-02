1ec696bf129f51d842ffe90fcea686c054341bea
sergeyt <stodyshev@gmail.com>
2015-10-02 20:16:46 +0600

    bump version

--- package.js
+++ package.js
@@ -2,11 +2,11 @@ Package.describe({
 	name: "sergeyt:typeahead",
 	summary: "Autocomplete package for meteor powered by twitter typeahead.js",
 	git: "https://github.com/sergeyt/meteor-typeahead.git",
-	version: "0.11.1_4"
+	version: "0.11.1_5"
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('METEOR@1.0');
+	api.versionsFrom('METEOR@1.2');
 	api.use(['jquery', 'blaze@1.0.0 || 2.0.0', 'ui'], 'client');
 	api.use('twbs:bootstrap@3.0.0', 'client', {weak: true});
 	api.addFiles('typeahead.bundle.js', 'client');
