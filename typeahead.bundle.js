174586f2e465afffb30bb9505f6333ac36f4a0a8
Kristijan Husak <husakkristijan@gmail.com>
2016-01-15 12:38:41 +0100

    Remove spellcheck attribute.

--- typeahead.bundle.js
+++ typeahead.bundle.js
@@ -2396,7 +2396,6 @@
         }
         function buildHintFromInput($input, www) {
             return $input.clone().addClass(www.classes.hint).removeData().css(www.css.hint).css(getBackgroundStyles($input)).prop("readonly", true).removeAttr("id name placeholder required").attr({
-                spellcheck: "false",
                 tabindex: -1
             });
         }
@@ -2407,9 +2406,7 @@
                 spellcheck: $input.attr("spellcheck"),
                 style: $input.attr("style")
             });
-            $input.addClass(www.classes.input).attr({
-                spellcheck: "false"
-            });
+            $input.addClass(www.classes.input);
             try {
                 !$input.attr("dir") && $input.attr("dir", "auto");
             } catch (e) {}
