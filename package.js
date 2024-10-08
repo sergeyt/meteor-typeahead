a4a3b6917eb8cd35e8a339835e1f6b8aa50d84c6
Sergey Todyshev <stodyshev@gmail.com>
2024-10-08 10:07:49 +0530

    fix: make it work for meteor v2
    also bump jquery 3.0.2

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom(['2.8.1', '3.0']);
-	api.use(['jquery@3.0.1', 'blaze@3.0.0'], 'client');
+	api.use(['jquery@3.0.2', 'blaze@2.9.0||3.0.0'], 'client');
 	api.use('twbs:bootstrap@3.3.6', 'client', {weak: true});
 	api.addFiles('typeahead.bundle.js', 'client');
 	api.addFiles('index.js', 'client');
